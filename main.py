1fddbae11302ed9b36fe2f639d0cb37adc45309b
TeamOpus <srvopus@gmail.com>
2025-09-18 14:14:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -612,4 +612,4 @@ async def stats():
 # ------------- Entrypoint -------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", "8000")), reload=True)
+    uvicorn.run("main:app", host="72.60.108.192", port=int(os.environ.get("PORT", "8000")), reload=True)
